c6e6d39e21d355120095cec72ff0cf1804af3201
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2021-10-04 18:03:12 +0530

    Hashir

--- jimbrootan.js
+++ jimbrootan.js
@@ -15,7 +15,7 @@ module.exports = {
  OA_AI: process.env.JINN_AI === undefined ? true : process.env.BOTNAME,  
  OB_NAME: process.env.BOT_NAME === undefined ? 'Jimbrootan' : process.env.BOT_NAME,
  OA_NAME: process.env.DEP_NAME === undefined ? 'Jimbrootan' : process.env.DEP_NAME,
- OA_REPLY: process.env.DEP_REPLY === undefined ? 'Mikhaiel is my creator' : process.env.DEP_REPLY,
+ OA_REPLY: process.env.DEP_REPLY === undefined ? 'Hashir is my creator' : process.env.DEP_REPLY,
     
 
 };
